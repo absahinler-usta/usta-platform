6a2f36a604215b1ab013c51b32828c2067e657ae
absahinler-usta <ab.sahinler@gmail.com>
2026-06-25 06:40:50 +0300

    PostgreSQL desteği eklendi

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,31 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false),
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          rejectUnauthorized: false,
+        },
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    debug: false,
-  },
-});
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
